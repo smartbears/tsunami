c812e2b8ca9c7f37a765854abcb0d9b986b17fa7
Javier de Paula <javi830810@gmail.com>
2014-12-04 11:34:09 -0500

    Another view, for hello world

--- src/shell/assets/js/shell.js
+++ src/shell/assets/js/shell.js
@@ -1,13 +1,35 @@
+
 App = Em.Application.create({
   rootElement: $('.view-container'),
 });
 
 App.Router.map(function() {
-  // put your routes here
+    this.route("mongo", { path: "/mongo" });
 });
 
 App.IndexRoute = Ember.Route.extend({
   setupController: function(controller, model) {
     controller.set('model', ['a', 'b', 'c']);
   }
+});
+
+App.MongoSample = Ember.Object.extend();
+App.MongoSample.reopenClass({
+    all: function() {
+        return $.getJSON("http://invite-voiceflows.appspot.com/api/invite/a59ba960e8f148fab76658b4724b982e").then(function(response) {
+            var items = [];
+            response.contacts.forEach( function (item) {
+                items.push( App.MongoSample.create(item) );
+            });
+            console.log(items);
+            return items;
+        });
+    }
+});
+
+
+App.MongoRoute = Ember.Route.extend({
+    model: function() {
+        return App.MongoSample.all();
+    }
 });
