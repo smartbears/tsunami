8b6df44926c0326b73d6a4a76db28cf828f41e16
javi830810 <javier.depaula@kaplan.edu>
2014-12-05 15:01:45 -0500

    Insert/Read interaction

--- src/shell/assets/js/shell.js
+++ src/shell/assets/js/shell.js
@@ -4,7 +4,8 @@ App = Em.Application.create({
 });
 
 App.Router.map(function() {
-    this.route("mongo", { path: "/mongo" });
+    this.route("read", { path: "/read" });
+    this.route("insert", { path: "/insert" });
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -16,20 +17,44 @@ App.IndexRoute = Ember.Route.extend({
 App.MongoSample = Ember.Object.extend();
 App.MongoSample.reopenClass({
     all: function() {
-        return $.getJSON("http://localhost:8081/api/home").then(function(response) { 
+        return $.getJSON("http://localhost:8081/api/home").then(function(response) {
             var items = [];
             response.forEach( function (item) {
                 items.push( App.MongoSample.create(item) );
             });
-            console.log(items);
+
             return items;
         });
+    },
+    one: function(){
+        return App.MongoSample.create({name: "no-name", age:0});
+    },
+    save: function(user){
+        //Save the user to the backend
+        console.log(user);
     }
 });
 
-
-App.MongoRoute = Ember.Route.extend({
+App.ReadRoute = Ember.Route.extend({
     model: function() {
         return App.MongoSample.all();
+    }
+});
+
+App.InsertRoute = Ember.Route.extend({
+    model: function() {
+        return App.MongoSample.one();
+    },
+    setupController: function(controller, model) {
+        controller.set('model', model);
+    }
+});
+
+App.InsertController = Ember.ObjectController.extend({
+    actions: {
+        insert_user: function() {
+
+            App.MongoSample.save(this.get('model'));
+        }
     }
 });
