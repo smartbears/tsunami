11c6c0b1ae20de100e284dd8aadb7db4a11c66c4
ajadex <ajadex@gmail.com>
2014-12-06 01:22:58 -0500

    stub controller UsersController

---
+++ src/protocols/Protocols.Runner/Controllers/UsersController .cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver.Builders;
+using Protocols.Runner.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Protocols.Runner.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public List<User> Get()
+        {            
+            return new List<User>(){new User{name = "jorge"}};
+        }
+
+    }
+}
