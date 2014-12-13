b7d62c6a35c5a688069c32c341bca4d42318e18e
ajadex <ajadex@gmail.com>
2014-12-12 23:52:01 -0500

    Enabling the RESTAdapter

--- src/shell/assets/js/models/shell.store.js
+++ src/shell/assets/js/models/shell.store.js
@@ -2,10 +2,10 @@
     host: 'http://localhost:8082/api',
 });
 */
-/*App.ApplicationAdapter = DS.WebAPIAdapter.extend({
+App.ApplicationAdapter = DS.WebAPIAdapter.extend({
     host: 'http://localhost:8082/api',
-});*/
-App.ApplicationAdapter = DS.FixtureAdapter;
+});
+//App.ApplicationAdapter = DS.FixtureAdapter;
 
 App.Store = DS.Store.extend({
     //adapter: DS.RESTAdapter.create({ bulkCommit: false, url: "http://localhost:8082"})
