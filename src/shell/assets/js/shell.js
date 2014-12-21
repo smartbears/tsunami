dd0713aa43e5e0ae36ae75a7f4e76660f1564548
ajadex <ajadex@gmail.com>
2014-12-21 13:47:42 -0500

    drag and drop example

--- src/shell/assets/js/shell.js
+++ src/shell/assets/js/shell.js
@@ -17,13 +17,15 @@ App.Router.map(function() {
         });
         this.route("add");
     });
+
+    this.resource("dnd");
 });
 
 App.SubjectsRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('subject');
     }
-}); 
+});
 
 App.SubjectRoute = Ember.Route.extend({
     model: function(params) {
@@ -36,7 +38,7 @@ App.AlergiesRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('alergy');
     }
-}); 
+});
 
 App.AlergyRoute = Ember.Route.extend({
     model: function(params) {
@@ -44,3 +46,46 @@ App.AlergyRoute = Ember.Route.extend({
     }
 });
 
+App.DndRoute = Ember.Route.extend({
+  model: function(){
+    return {
+      inFolder: [
+        {id: 1, name: "File 1"},
+        {id: 2, name: "File 2"},
+        {id: 3, name: "File 3"}
+      ],
+      inTrash: []
+    };
+  }
+});
+
+App.DraggableFileComponent = Ember.Component.extend({
+  dragStart: function(event){
+    event.dataTransfer.setData('text/data', this.get('file.id'));
+  }
+});
+
+App.DroppableTrashComponent = Ember.Component.extend({
+  overTrash: false,
+
+  dragEnter: function(){
+    this.set('overTrash', true);
+  },
+
+  dragLeave: function(){
+    this.set('overTrash', false);
+  },
+
+  dragOver: function(event){
+    event.preventDefault();
+  },
+
+  drop: function(event){
+    id = event.dataTransfer.getData('text/data');
+    record = this.get('inFolder').findProperty('id', parseInt(id));
+    this.set('overTrash', false);
+    this.get('inTrash').pushObject(record);
+    this.get('inFolder').removeObject(record);
+  }
+
+});
