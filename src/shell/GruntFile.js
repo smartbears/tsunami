3e1f297117723ed4f116902ebcaa2a0406e505fc
javi830810 <javi830810+tsunami@gmail.com>
2015-01-12 23:40:30 -0500

    Including Partials

--- src/shell/GruntFile.js
+++ src/shell/GruntFile.js
@@ -92,8 +92,8 @@ module.exports = function (grunt) {
                     'assets/js/shell.js',
                     'assets/js/views/**/*.js',
                     'assets/js/models/*.js',
-                    'assets/js/components/*.js',
-			'assets/js/controllers/**/*.js',
+                    'assets/js/components/**/*.js',
+                    'assets/js/controllers/**/*.js',
                 ],
                 dest: 'assets/js/shell.min.js'
 
@@ -170,7 +170,15 @@ module.exports = function (grunt) {
                 tasks: ['emberTemplates']
             },
             application_scripts: {
-                files: ['assets/js/*.js', 'assets/js/mixins/*.js', 'assets/js/models/**/*.js', 'assets/js/views/**/*.js', 'assets/js/controllers/*.js', 'assets/js/components/*.js','!assets/js/*.min.js'],
+                files: [
+                            'assets/js/*.js',
+                            'assets/js/mixins/*.js',
+                            'assets/js/models/**/*.js',
+                            'assets/js/views/**/*.js',
+                            'assets/js/controllers/*.js',
+                            'assets/js/components/*.js',
+                            '!assets/js/*.min.js'
+                ],
                 tasks: [
                     'uglify:application_scripts'
                 ]
