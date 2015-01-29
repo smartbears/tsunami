62b2bb556f3af3e4f248f9f24e87acc09e80e883
Jorge <jotolo7@gmail.com>
2015-01-28 21:13:37 -0800

    checking existency of procedures into visit when drag a procedure

--- src/shell/assets/js/protocolApp/controllers/visitController.js
+++ src/shell/assets/js/protocolApp/controllers/visitController.js
@@ -4,12 +4,29 @@ ProtocolApp.VisitController = Ember.ObjectController.extend({
   	actions: {
     	drop_procedure_into_visit: function(procedureId){
     		var procedure = this.get('controllers.procedures').store.getById('procedure',procedureId);
-	    	if(!this.get("procedures"))
-	    	    this.set("procedures", [procedure]);
-	    	else
-	    		this.get("procedures").pushObject(procedure);
+	        var exist = false;
+
+	        if(!this.get("procedures"))
+		    	this.set("procedures", [procedure]);
+		    else{
+		    	
+		    	var procedures = this.get("procedures");
+		    	for (var i = 0; i < procedures.length; i++) {
+		    		if(procedures[i].get('id') === procedure.get('id')){
+		    			exist = true;
+		    			break;
+		    		}
+		    	};		    		
+
+		    	if(exist){
+		    		alert('This element already exist in this visit.');
+		    	}
+		    	else{
+		    		this.get('procedures').pushObject(procedure);
+		    	}
+		    }
    	 	},
-   	 	
+
    	 	editLabel: function () {
 	      this.set("isEditingLabel", true);       
 	    },
