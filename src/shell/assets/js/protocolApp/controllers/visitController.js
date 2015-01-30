d97f18cff36f4ca052ebe0509e9021cc453a2d42
ajadex <ajadex@gmail.com>
2015-01-30 00:06:55 -0500

    Correcting the visit model to have a collection of ids and represent the procedures in the controller instead.

--- src/shell/assets/js/protocolApp/controllers/visitController.js
+++ src/shell/assets/js/protocolApp/controllers/visitController.js
@@ -1,12 +1,27 @@
 ProtocolApp.VisitController = Ember.ObjectController.extend({
-	needs: 'procedures',
+	needs: 'protocolsConfigure',
   	actions: {
     	drop_procedure_into_visit: function(procedureId){
-    		var procedure = this.get('controllers.procedures').store.getById('procedure',procedureId);
-	    	if(!this.get("procedures"))
-	    	    this.set("procedures", [procedure]);
-	    	else
-	    		this.get("procedures").pushObject(procedure);
-   	 	}
-  	}
+    		var procedure = this.get('controllers.protocolsConfigure')
+														.get('procedures')
+														.findBy("id", procedureId);
+
+				if(!this.model.get("procedureIds"))
+					this.model.set("procedureIds", [procedure.id]);
+				else
+					this.model.get("procedureIds").pushObject(procedure.id);
+		 	}
+  	},
+
+		procedures: function(){
+			var procIds = this.model.get("procedureIds");
+			if(!procIds)
+				return [];
+
+			return this.get('controllers.protocolsConfigure')
+					.get('procedures')
+					.filter(function(p){
+						return procIds.contains(p.id)
+					});
+		}.property("model.procedureIds.@each")
 });
--- src/shell/assets/js/protocolApp/models/visit.js
+++ src/shell/assets/js/protocolApp/models/visit.js
@@ -1,5 +1,5 @@
 ProtocolApp.Visit = DS.Model.extend({
   label: DS.attr('string'),
   number: DS.attr('number'),
-  //procedures: DS.hasMany('procedure')
+  procedureIds: DS.attr(), //array of proc ids.
 });
