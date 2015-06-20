e3c5caf74d299c830a89b4ae6ab1621623b75e84
Jorge Tomas <jotolo7@gmail.com>
2015-06-20 14:08:09 -0700

    adding selectView.js

---
+++ src/shell/assets/js/subjectApp/components/selectView.js
@@ -0,0 +1,12 @@
+SubjectApp.SelectView = Ember.Select.extend({
+	change: function(){
+		var self = this;
+		this._super();
+		var callback = this.get('onChange');
+		if(callback){
+			Em.run.later(function(){
+				self.get('controller').send(callback);
+			});
+		}
+	}
+});
